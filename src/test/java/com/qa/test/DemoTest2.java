de92241d6fab3b996087ccf2293ab524e49aa06b
Sagar R <sagar1995bc@gmail.com>
2022-03-04 16:03:14 +0530

    first git commit for my selenium code

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,45 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+//demo unit test cases 
+//If execute through Maven test - happens direclty 
+	@Test
+	public void sum() {
+		System.err.println("SUM");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a + b);
+
+	}
+
+	@Test
+	public void sub() {
+		System.err.println("SUB");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b - a);
+
+	}
+
+	@Test
+	public void multi() {
+		System.err.println("MULTIPLICATION");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a * b);
+
+	}
+
+	@Test
+	public void div() {
+		System.err.println("DIV");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b / a);
+
+	}
+
+}
